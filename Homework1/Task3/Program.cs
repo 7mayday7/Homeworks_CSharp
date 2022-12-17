2f585d4ae2476dae564ef1efc661dbf6f6b0f501
denis <stroodenis462@mail.ru>
2022-12-17 22:58:24 +0600

    Task 3 solved

---
+++ Homework1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Console.WriteLine("Программа определения четности числа");
+
+Write("Введите целое число: ");
+int num = Convert.ToInt32(ReadLine());
+
+if (num % 2 == 0)
+{
+    WriteLine($"Число {num} — четное");
+}
+else
+{
+    WriteLine($"Число {num} — нечетное");
+}
