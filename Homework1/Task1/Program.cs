fcacdf3506d3cfbcedb291e3a6c0324c299362ea
denis <stroodenis462@mail.ru>
2022-12-17 22:55:32 +0600

    Task 1 solved

---
+++ Homework1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+Console.WriteLine("Программа нахождения наибольшего из двух чисел");
+
+Write("a = ");
+int a = Convert.ToInt32(ReadLine());
+
+Write("b = ");
+int b = Convert.ToInt32(ReadLine());
+
+if(a > b)
+{
+    WriteLine($"Наибольшее число: {a}");
+}
+else if (b > a)
+{
+    WriteLine($"Наибольшее число: {b}");
+}
+else
+{
+    WriteLine("Числа равны!");
+}
