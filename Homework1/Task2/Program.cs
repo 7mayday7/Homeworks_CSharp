4d72404f6ef39c50e11b8329a2972c26d311487e
denis <stroodenis462@mail.ru>
2022-12-17 22:57:06 +0600

    Task 2 solved

---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Console.WriteLine("Программа нахождения наибольшего из трех чисел");
+
+Write("a = ");
+int a = Convert.ToInt32(ReadLine());
+
+Write("b = ");
+int b = Convert.ToInt32(ReadLine());
+
+Write("c = ");
+int c = Convert.ToInt32(ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Наибольшее число: {max}");
