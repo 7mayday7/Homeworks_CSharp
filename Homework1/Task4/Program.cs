0d63f1078de0229820274f65868ebb18e0e320ca
denis <stroodenis462@mail.ru>
2022-12-17 22:59:41 +0600

    Task 4 solved

---
+++ Homework1/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Console.WriteLine("Программа поиска четных чисел в диапазоне от 1 до n");
+
+Write("n = ");
+int n = Convert.ToInt32(ReadLine());
+
+int begin = 1;
+
+while (begin <= n)
+{
+    if (begin % 2 == 0)
+    {
+        WriteLine(begin);
+    }
+    begin += 1;
+}
