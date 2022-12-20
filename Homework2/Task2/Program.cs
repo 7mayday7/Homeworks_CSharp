eacf37dfef9f28a63410600224b3e5bb5daf84fe
denis <stroodenis462@mail.ru>
2022-12-20 22:27:25 +0600

    Task 2 homework 2 solved

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine());
+
+int result;
+
+if (num >= 100)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    result = num % 10;
+    WriteLine($"Третья цифра числа: {result}");
+}
+else
+{
+    WriteLine("Третьей цифры нет");
+}
