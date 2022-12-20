e585a6853dc975f31bb8ea7a8386c7a44694df96
denis <stroodenis462@mail.ru>
2022-12-20 22:30:05 +0600

    Task 3 homework 2 solved

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(ReadLine());
+
+WriteLine(day >= 1 && day <= 7 ? day == 6 || day == 7 ? "Выходной день" : "Будний день" : "Несуществующий день недели");
