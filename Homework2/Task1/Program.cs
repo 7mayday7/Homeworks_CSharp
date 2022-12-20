90fcb854a98a0ecd7403e37630b5666214fc8dfb
denis <stroodenis462@mail.ru>
2022-12-20 22:22:18 +0600

    Task 1 homework 2 solved

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(ReadLine());
+
+int second_digit = num % 100 / 10;
+
+WriteLine($"Вторая цифра числа: {second_digit}");
