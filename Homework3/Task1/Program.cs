2f8536824ada7a461fbf6d11dffbf0ae84e40861
denis <stroodenis462@mail.ru>
2022-12-24 19:41:40 +0600

    Task 1 homework 3 solved

---
+++ Homework3/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+
+if (!Int32.TryParse(ReadLine(), out int num))
+{
+    WriteLine("Ошибка! Введено не число!");
+    return;
+}
+
+int remainder;
+int sum = 0;
+int temp = num;
+
+while (num > 0)
+{
+    remainder = num % 10;
+    sum = (sum * 10) + remainder;
+    num = num / 10;
+}
+
+Console.WriteLine(temp == sum ? $"Число {temp} - палиндром" : $"Число {temp} - не палиндром");
