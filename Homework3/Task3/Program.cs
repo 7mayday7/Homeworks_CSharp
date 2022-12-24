9e3980aada875d4fce83970bea1f3f2f08338423
denis <stroodenis462@mail.ru>
2022-12-24 20:27:37 +0600

    Task 3 homework 3 solved

---
+++ Homework3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Таблица кубов от 1 до N");
+
+Write("Введите N: ");
+if (!int.TryParse(ReadLine(), out int N))
+{
+    WriteLine("Ошибка! Введено не число!");
+    return;
+}
+
+int i = 1;
+
+if (N > 0)
+{
+    while (i <= N)
+    {
+        Write($"{Math.Pow(i, 3)} ");
+        i++;
+    }
+}
+else
+{
+    while (i >= N)
+    {
+        Write($"{Math.Pow(i, 3)} ");
+        i--;
+    }
+}
