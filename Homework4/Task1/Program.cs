de03a8aee7914eeb711d1c012c38f83909c9c9ea
denis <stroodenis462@mail.ru>
2023-01-17 14:51:36 +0600

    Task 1 homework 4 solved

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+Write("Введите степень: ");
+int degree = int.Parse(ReadLine());
+
+int answer = PovNum(number, degree);
+
+WriteLine($"Число {number} в степени {degree} = {answer}");
+
+int PovNum(int num, int pov)
+{
+    int result = 1;
+
+    if (pov == 0) return 1;
+
+    for (int i = 0; i < pov; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
