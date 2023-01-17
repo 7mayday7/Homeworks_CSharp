14bec5e1a4be6c29721000e16ba5793bed654eb4
denis <stroodenis462@mail.ru>
2023-01-17 14:55:30 +0600

    Task 3 homework 4 solved

---
+++ Homework4/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Clear();
+
+GetArray();
+
+int[] GetArray()
+{
+    Write("[");
+    int[] array = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        array[i] = random.Next(1, 100);
+        Write($"{array[i]}, ");
+    }
+
+    WriteLine($"{array[array.Length - 1] = random.Next(1, 100)}]");
+    return array;
+}
