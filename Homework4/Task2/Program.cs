f3f1defad0631e00c054d6d672b13e0b9e258ab2
denis <stroodenis462@mail.ru>
2023-01-17 14:54:11 +0600

    Task 2 homework 4 solved

---
+++ Homework4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+
+int answer = SumDigit(number);
+
+WriteLine($"Сумма цифр в числе {number} = {answer}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
