a495d0acdcd4603f62fc34435ae6e03300abf64e
denis <stroodenis462@mail.ru>
2023-01-23 19:28:11 +0600

    Task 1 homework 6 solved

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Ведите размер массива: ");
+int arraySize = int.Parse(ReadLine()!);
+int[] array = GetArray(arraySize);
+
+WriteLine($"Получившийся массив: [{String.Join(", ", array)}]");
+
+int countPositive = FindPositiveNum(array);
+
+WriteLine($"Количество положительных чисел: {countPositive}");
+
+int[] GetArray(int size)
+{
+    Console.Write($"Введите {size} чисел через пробел: ");
+    string M = Console.ReadLine()!;
+    string[] stringArray = M.Split(' ');
+    int[] resultArray = new int[size];
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = int.Parse(stringArray[i]);
+    }
+    return resultArray;
+}
+
+int FindPositiveNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
