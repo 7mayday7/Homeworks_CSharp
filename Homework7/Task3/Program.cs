232c592c151d6be45fa1faab6164533a26df2854
denis <stroodenis462@mail.ru>
2023-01-27 23:12:01 +0600

    Task 3 homework 7 solved

---
+++ Homework7/Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+PrintMatrixArray(array);
+WriteLine();
+getArrayOfAverages(array);
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5} ");
+        }
+        WriteLine();
+    }
+}
+
+void getArrayOfAverages(int[,] intArray)
+{
+    double[] columnAverages = new double[intArray.GetLength(1)];
+
+    for (int j = 0; j < intArray.GetLength(1); j++)
+    {
+        double currentSum = 0;
+        for (int i = 0; i < intArray.GetLength(0); i++)
+        {
+            currentSum += intArray[i, j];
+        }
+        columnAverages[j] = currentSum / intArray.GetLength(0);
+    }
+
+    for (int i = 0; i < columnAverages.Length; i++)
+    {
+        Write($"{columnAverages[i]}; ");
+    }
+}
