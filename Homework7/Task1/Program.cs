d35f9d51887fa5ceb867c77f016fa9ebf3651584
denis <stroodenis462@mail.ru>
2023-01-27 23:07:14 +0600

    Task 1 homework 7 solved

---
+++ Homework7/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите размер матрицы через пробел: ");
+string[] parameters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+double[,] array=GetMatrixArray(int.Parse(parameters[0]),int.Parse(parameters[1]));
+PrintMatrixArray(array);
+
+double[,] GetMatrixArray(int rows, int columns)
+{
+    double[,] resultArray=new double[rows,columns];
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i,j] = Math.Round(random.NextDouble() * 10, 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j],6} ");
+        }
+        WriteLine();
+    }
+}
