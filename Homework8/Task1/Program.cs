69fb188e3eb8b87fb4905acbba45266de9f18cf7
denis <stroodenis462@mail.ru>
2023-01-31 23:24:04 +0600

    Task 1 homework 8 solved

---
+++ Homework8/Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetMatrixArray(int.Parse(parameters[0]),
+                              int.Parse(parameters[1]),
+                              int.Parse(parameters[2]),
+                              int.Parse(parameters[3]));
+
+PrintMatrixArray(array);
+
+int[,] matrix = SortDesc(array);
+
+WriteLine();
+PrintMatrixArray(matrix);
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] SortDesc(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(1) - 1; k++)
+            {
+                if (inArray[i, k] < inArray[i, k + 1])
+                {
+                    int temp = inArray[i, k];
+                    inArray[i, k] = inArray[i, k + 1];
+                    inArray[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return inArray;
+}
