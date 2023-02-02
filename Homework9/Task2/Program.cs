ec39d1d6254a3b34ae446d7db57a7bec684814bd
denis <stroodenis462@mail.ru>
2023-02-02 13:59:11 +0600

    Task 2 homework 9 solved

---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+Write("M = ");
+int m = int.Parse(Console.ReadLine()!);
+
+Write("N = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int result = GetSum(m, n);
+
+WriteLine($"\nСумма натуральных элементов в промежутке от {m} до {n} = {result}");
+
+int GetSum(int m, int n)
+{
+    return m == n ? m : n + GetSum(m, n - 1);
+}
