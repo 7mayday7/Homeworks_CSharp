20cf3e50ed0690731cbdf6e914850cda024445e1
denis <stroodenis462@mail.ru>
2023-02-02 13:57:44 +0600

    Task 1 homework 9 solved

---
+++ Homework9/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write("N = ");
+int N = int.Parse(Console.ReadLine()!);
+WriteLine($"\nНатуральные числа в промежутке от {N} до 1: \n{GetNumbers(N)}");
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{num}, {GetNumbers(num - 1)}";
+}
