a16fe6b3b5ee2c919d6922c06740effa27f8fcea
denis <stroodenis462@mail.ru>
2023-02-02 14:00:31 +0600

    Task 3 homework 9 solved

---
+++ Homework9/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+Write("m = ");
+int m = int.Parse(Console.ReadLine()!);
+
+Write("n = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int result = Ackermann(m, n);
+
+WriteLine($"\nРезультат вычисления функции Аккермана: {result}");
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Ackermann(m - 1, 1);
+
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
